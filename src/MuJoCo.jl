42cf150b64f832f385124c1f6de1b11860829e2e
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2022-09-17 19:08:42 -0400

    init package

--- src/MuJoCo.jl
+++ src/MuJoCo.jl
@@ -1,5 +1,17 @@
 module MuJoCo
 
-# Write your package code here.
+using PythonCall
+
+const dm_control = PythonCall.pynew()
+const mujoco = PythonCall.pynew()
+
+function __init__()
+    PythonCall.pycopy!(dm_control, pyimport("dm_control"))
+    PythonCall.pycopy!(mujoco, pyimport("dm_control.mujoco"))
+end
+
+export dm_control,
+       mujoco
+
 
 end
